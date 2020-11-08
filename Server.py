6eab67d8451065bbb592eab768e529d965fa1605
Dikshant Raj <44780302+dikshantraj09@users.noreply.github.com>
2020-11-08 15:06:39 +0530

    Add files via upload

--- Server.py
+++ Server.py
@@ -5,33 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import animation as animation
-import pyautogui as pa
 HEADER_LENGTH = 10
 
 IP = "0.0.0.0"
 PORT = 1234
 x_arr, y_arr, z_arr = [], [], []
 x_gry, y_gry, z_gry = [], [], []
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# SO_ - socket option
-# SOL_ - socket option level
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Sets REUSEADDR (as a socket option) to 1 on socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind, so server informs operating system that it's going to use given IP and port
-# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
 server_socket.bind((IP, PORT))
-
 # This makes server listen to new connections
 server_socket.listen()
-
 # List of sockets for select.select()
 sockets_list = [server_socket]
-
 # List of connected clients - socket as a key, user header and name as data
 clients = {}
 
@@ -56,24 +45,11 @@ def receive_message(client_socket):
         return {'header': message_header, 'data': client_socket.recv(message_length)}
 
     except:
-
-        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
-        # or just lost his connection
-        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
-        # and that's also a cause when we receive an empty message
         return False
-fig = plt.figure(figsize=(800,800))
-while True:
 
-    # Calls Unix select() system call or Windows select() WinSock call with three parameters:
-    #   - rlist - sockets to be monitored for incoming data
-    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
-    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
-    # Returns lists:
-    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
-    #   - writing - sockets ready for data to be send thru them
-    #   - errors  - sockets with some exceptions
-    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
+fig = plt.figure()
+
+while True:
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
     
@@ -84,8 +60,6 @@ while True:
         if notified_socket == server_socket:
 
             # Accept new connection
-            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
-            # The other returned object is ip/port set
             client_socket, client_address = server_socket.accept()
 
             # Client should send his name right away, receive it
@@ -161,7 +135,7 @@ while True:
             #d_lz = [sum(v_lz[:j]) * time_between_samples for j in range(len(v_lz))]                 
             
             i = list(range(len(x_arr)))
-            
+           
         
             ax = plt.axes(projection='3d')
             ax.set_xlim3d(-10,10)
